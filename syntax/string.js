173136b6325efd11ac1ba3e1803829d074af215b
GyuHyeonWkdWkdMan <nero0213@naver.com>
2023-10-19 19:20:57 +0900

    node

---
+++ syntax/string.js
@@ -0,0 +1,3 @@
+console.log(1+1);
+console.log('1'+'1'+'d');
+console.log('김철수'.length);
---
+++ syntax/variable.js
@@ -0,0 +1,7 @@
+a = 1;
+console.log(a);
+a = 2;
+console.log(a);
+var st = `dsafdsaf
+dsafasdfasdfsadf`
+console.log(st);
